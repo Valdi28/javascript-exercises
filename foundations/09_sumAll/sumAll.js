aaaf6d81b775b5ecb4b49915f0ef1d1af467e163
Valdi28 <106452779+Valdi28@users.noreply.github.com>
2026-04-26 23:07:32 +0200

    Complete foundations 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR"
+    }
 
+    if (a < 0 || b < 0) {
+        return "ERROR"
+    }
+
+    let largestNumber;
+    let smallestNumber;
+
+    if (a > b) {
+        largestNumber = a;
+        smallestNumber = b
+    } else {
+        largestNumber = b;
+        smallestNumber = a;
+    }
+
+
+    let total = 0;
+    for (let i = smallestNumber; i <= largestNumber; i++) {
+        total += i;
+    }
+
+    return total;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
