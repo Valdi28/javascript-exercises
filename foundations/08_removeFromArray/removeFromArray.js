5dce037070a91adcf5df829152db26f189af9699
Valdi28 <106452779+Valdi28@users.noreply.github.com>
2026-04-26 22:47:18 +0200

    Complete foundations 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    array = [...array]
+
+    const args = Array.from(arguments)
+    args.map((item, index) => {
+        if (index === 0) return;
+
+        while (array.includes(item)) {
+            const itemToRemoveIndex = array.indexOf(item);
+            console.log(array, item, itemToRemoveIndex);
+            
+            array.splice(itemToRemoveIndex, 1)
+        } 
+    })
+
+    return array
 };
+
+console.log(removeFromArray([1, 2, 2, 3], 2));
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
