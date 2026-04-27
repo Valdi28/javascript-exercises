c9af55274c997ea0446102e61cce2c29ac808905
Valdi28 <106452779+Valdi28@users.noreply.github.com>
2026-04-27 16:32:01 +0200

    Complete foundations 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const temperatureInCelsius = (temperature - 32) * 5/9
+
+  return Math.round(temperatureInCelsius * 10) / 10
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const temperatureInFarenheit = temperature * 9/5 + 32
+
+  return Math.round(temperatureInFarenheit * 10) / 10
 };
 
 // Do not edit below this line
