b111ad79c49d704cabdca04371daf8d840fb5b69
Valdi28 <106452779+Valdi28@users.noreply.github.com>
2026-06-05 21:20:41 +0200

    Complete foundations 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const characters = "abcdefghijklmnopqrstuvwxyz1234567890"
+    const lowercase =  string.toLowerCase()
 
+    const formatedString = lowercase.split("").filter(char => characters.split("").includes(char)).join("");
+
+
+    const reservedString = formatedString.split("").reverse().map(char => char).join("")
+    return (formatedString == reservedString)
 };
+
+console.log(palindromes('A car, a man, a maraca.'));
+
 
 // Do not edit below this line
 module.exports = palindromes;
