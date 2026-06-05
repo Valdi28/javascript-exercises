daddb38324cf67f263c46a9d07240fe24756d4bd
Valdi28 <106452779+Valdi28@users.noreply.github.com>
2026-06-05 21:33:49 +0200

    Complete foundation 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if (position < 0) {
+        return "OOPS"
+    }
 
+    const fibonacci = [0, 1, 1]
+    for (let i = 2; i < position; i++) {
+        fibonacci.push(fibonacci[i]+fibonacci[i-1]);
+    }
+
+    return fibonacci[position]
 };
+
+console.log(fibonacci(40));
 
 // Do not edit below this line
 module.exports = fibonacci;
