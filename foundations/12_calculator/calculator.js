9c8295d44c7a1b01be7d290c5a5a97fe48778990
Valdi28 <106452779+Valdi28@users.noreply.github.com>
2026-06-05 20:54:48 +0200

    Complete foundations 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, number) => {
+    return acc += number
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, number) => {
+    return acc *= number;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  return base**exp;
 };
 
-const factorial = function() {
-	
+
+const factorial = function(number) {
+	// 10! = 10*9*8*7*6*5*4*3*2*1
+  let factorial = 1;
+
+  for (let i = 0; i<number; i++) {
+    factorial *= (number - i);
+  }
+
+  return factorial
 };
+
+console.log(factorial(4));
+
 
 // Do not edit below this line
 module.exports = {
