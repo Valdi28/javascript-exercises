43d544b0a16f0347bdbc0a8d6d183a2076810c1e
Valdi28 <106452779+Valdi28@users.noreply.github.com>
2026-06-06 14:05:44 +0200

    Complete foundations 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
 
+    const peopleSortedByAge = peopleArr.sort((a, b) => {
+        let aAge;
+        let bAge;
+
+        if (a.yearOfDeath) {
+            aAge = a.yearOfDeath - a.yearOfBirth
+        } else {
+            aAge = new Date().getFullYear() - a.yearOfBirth
+            console.log(aAge);
+            
+        }
+
+        if (b.yearOfDeath) {
+            bAge = b.yearOfDeath - b.yearOfBirth
+        } else {
+            bAge = new Date().getFullYear() - b.yearOfBirth
+            
+        }
+
+        return (bAge - aAge)
+    })
+
+    return peopleSortedByAge[0]
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
